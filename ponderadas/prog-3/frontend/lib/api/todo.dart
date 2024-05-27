a261173064342c753dbfa080dfa9adb8a16871c8
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2024-05-27 05:58:02 -0300

    starting frontend pond3

---
+++ ponderadas/prog-3/frontend/lib/api/todo.dart
@@ -0,0 +1,77 @@
+import "dart:convert";
+import "dart:io";
+
+import "package:frontend/model/todo.dart";
+
+import "../globals.dart" as globals;
+import "package:http/http.dart" as http;
+
+var baseurl = globals.baseUrl;
+
+Future<List<ToDo>> getTodos() async {
+  final response = await http.get(
+    Uri.parse("$baseurl/todo/user:${globals.userId}"),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ${globals.accessToken}',
+    },
+  );
+  if (response.statusCode == 200) {
+    final List<dynamic> data = json.decode(response.body);
+    return data.map((d) => ToDo.fromJson(d)).toList();
+  } else {
+    return ToDo.todoList();
+  }
+}
+
+Future<bool> addTodo(String content) async {
+  final response = await http.post(Uri.parse("$baseurl/todo/"),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ${globals.accessToken}',
+      },
+      body: jsonEncode(<String, dynamic>{
+        "content": content,
+        "user_id": globals.userId,
+      }));
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Future<bool> removeTodo(int id) async {
+  final response = await http.delete(
+    Uri.parse("$baseurl/todo/$id"),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ${globals.accessToken}',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Future<bool> checkTodo(int id) async {
+  final response = await http.put(
+    Uri.parse("$baseurl/todo/check/$id"),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ${globals.accessToken}',
+    },
+  );
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ ponderadas/prog-3/frontend/lib/globals.dart
@@ -0,0 +1,7 @@
+bool isLoggedIn = false;
+
+int userId = -1;
+
+String accessToken = "";
+
+String baseUrl = "https://backend-pond-1.onrender.com";
